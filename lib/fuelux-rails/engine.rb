c89811baa0a38d41a15d03324108b3d4cfddada8
Michael McIntyre <michael@tekanics.com>
2013-01-25 13:09:07 -0500

    remove less dependency

--- lib/fuelux-rails/engine.rb
+++ lib/fuelux-rails/engine.rb
@@ -2,10 +2,5 @@ require 'rails'
 
 module FueluxRails
   class Engine < ::Rails::Engine
-    initializer 'fuelux-rails.setup', 
-      :after => 'less-rails.after.load_config_initializers', 
-      :group => :all do |app|
-      app.config.less.paths << File.join(config.root, 'vendor', 'toolkit')
-      end
   end
 end
